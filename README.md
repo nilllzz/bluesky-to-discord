597179b0f47d54987d45638c7ef365192c2d18aa
nilllzz <nidresch@googlemail.com>
2024-10-19 22:19:23 +0200

    Initialest commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # bluesky-to-discord
+
 Pushes new media posts from a Bluesky account to a Discord Webhook
+
+## How to run
+
+- Install NodeJS >= 20
+- Copy the .env file to .env.local
+  - Put the desired Bluesky at-handle for the `USER_HANDLE` variable
+  - Get your Discord webhook url (Server settings > Integrations > Webhooks) and place it for the `DISCORD_WEBHOOK_URL` variable
+- Run the script
+  - `node --env-file=.env.local index.js`
+
+The script will fetch the last 50 posts from the Bluesky profile defined in the env file.
+
+On the first run the script will not post anything to Discord and will record the newest post from the feed (saved to a file called `newest_post`).
+
+On subsequent runs, any newer posts will be submitted to Discord one by one.
+
+_Note_: Only posts by the given author that include media and are not replies will be posted.
---
+++ index.js
@@ -0,0 +1,116 @@
+const fs = require("node:fs/promises");
+
+/**
+ * @typedef {{handle:string}} Author
+ * @typedef {{text:string,reply:{}|undefined}} PostRecord
+ * @typedef {{uri:string,author:Author,record:PostRecord,indexedAt:string}} Post
+ * @typedef {Post[]} PostFeed
+ */
+
+const apiEndpoint =
+  "https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed";
+const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
+const userHandle = process.env.USER_HANDLE;
+const newestPostFilename = "newest_post";
+
+if (!webhookUrl || !userHandle) {
+  console.error("Missing environment variables");
+  process.exit(1);
+}
+
+async function main() {
+  const response = await fetch(
+    apiEndpoint + "?filter=posts_with_media&actor=" + userHandle
+  );
+
+  const json = await response.json();
+  /** @type {PostFeed} feed */
+  const feed = json.feed.map((postObj) => postObj.post);
+
+  // Only get posts that are from the user and not replies.
+  const ownerFeed = feed.filter(
+    (post) =>
+      post.author.handle === userHandle && post.record.reply === undefined
+  );
+  if (!ownerFeed.length) {
+    console.info("No posts found for the owner");
+    return;
+  }
+
+  const newestPost = ownerFeed[0];
+  const newestIndexedAt = new Date(newestPost.indexedAt);
+
+  // Check if the last_post file exists:
+  try {
+    await fs.access(newestPostFilename);
+  } catch (error) {
+    console.info("No last indexed post, write current post at", {
+      newestIndexedAt,
+    });
+    await writeLastPost(newestPost);
+    return;
+  }
+
+  // Get last indexed post and compare dates to latest post from feed.
+  const lastNewestPostData = await fs.readFile(newestPostFilename, {
+    encoding: "utf-8",
+  });
+  const lastNewestPost = JSON.parse(lastNewestPostData);
+  const lastNewestPostIndexedAt = new Date(lastNewestPost.indexedAt);
+
+  // Don't post if newest post on feed is not newer than the last posted one.
+  if (newestIndexedAt <= lastNewestPostIndexedAt) {
+    console.info("No new post since last check");
+    return;
+  }
+
+  // Write newest post to file.
+  await writeLastPost(newestPost);
+
+  // Go through the feed and submit every post that is newer than the last posted one.
+  let foundOlderPost = false;
+  do {
+    const newPost = ownerFeed.shift();
+    const newPostIndexedAt = new Date(newPost.indexedAt);
+    if (newPostIndexedAt <= lastNewestPostIndexedAt) {
+      foundOlderPost = true;
+      continue;
+    }
+
+    const postHash = newestPost.uri.split("/").pop();
+    const uri = "https://bsky.app/profile/" + userHandle + "/post/" + postHash;
+    const postText = newestPost.record.text;
+    let messageContent = uri;
+    if (postText) {
+      messageContent = postText + "\n" + messageContent;
+    }
+
+    console.info("Submit post to Discord", uri);
+
+    const postResponse = await fetch(webhookUrl, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        content: messageContent,
+      }),
+    });
+
+    if (postResponse.ok) {
+      console.info("  Posted to Discord");
+    } else {
+      console.error("  Failed to post to Discord", postResponse.statusText);
+    }
+  } while (!foundOlderPost && ownerFeed.length > 0);
+}
+
+/**
+ * @param {Post} post
+ */
+async function writeLastPost(post) {
+  await fs.writeFile("newest_post", JSON.stringify(post));
+}
+
+main();
